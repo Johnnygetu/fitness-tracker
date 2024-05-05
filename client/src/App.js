3f6792887de6ada464c1a29698bf1ff446b7e22b
yishak <yishak.110402@gmail.com>
2024-05-05 15:32:56 +0300

    added react

---
+++ client/src/App.js
@@ -0,0 +1,55 @@
+import './fitness.css'
+export default function App() {
+  return (
+   <>
+   <div class="card">
+      <div class="acc">
+        <span class="accLabel">Users</span>
+        <div class="accContainer">
+          <ul>
+            <li class="accList selectedUser">
+              <svg
+                class="profileImg"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+              >
+                <path
+                  d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"
+                /></svg>Johnny
+            </li>
+            <li class="accList">
+              <svg
+                class="profileImg"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+              >
+                <path
+                  d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"
+                /></svg>Yishak
+            </li>
+          </ul>
+          <svg
+            class="addAcc"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+          >
+            <path
+              d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"
+            />
+          </svg>
+        </div>
+      </div>
+      <div class="status">
+        <span class="name">Johnny</span>
+        <div class="bmiStatus"><b>BMI</b>: 22</div>
+        <div class="otherStatus">
+          <div class="ageStatus"><b>Age</b>: 20</div>
+          <div class="heightStatus"><b>Height</b>: 1.75m</div>
+          <div class="weightStatus"><b>Weight</b>: 71kg</div>
+        </div>
+      </div>
+    </div>
+   </>
+  );
+}
+
